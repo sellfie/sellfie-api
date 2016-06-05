53960bc7c6a1573a94dc607ab9b88702ae4c883b
Mai Anh Vu <me@maianhvu.com>
2016-06-05 14:17:31 +0800

    modify gems and add config for local machines

--- config/puma.rb
+++ config/puma.rb
@@ -1,29 +1,48 @@
-# Change to match your CPU core count
-workers 2
+if ENV['RAILS_ENV'].eql? 'production'
+  # Change to match your CPU core count
+  workers 2
 
-# Min and Max threads per worker
-threads 1, 6
+  # Min and Max threads per worker
+  threads 1, 6
 
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
+  app_dir = File.expand_path("../..", __FILE__)
+  shared_dir = "#{app_dir}/shared"
 
-# Default to production
-rails_env = ENV['RAILS_ENV'] || "production"
-environment rails_env
+  # Default to production
+  rails_env = ENV['RAILS_ENV'] || "production"
+  environment rails_env
 
-# Set up socket location
-bind "unix://#{shared_dir}/sockets/puma.sock"
+  # Set up socket location
+  bind "unix://#{shared_dir}/sockets/puma.sock"
 
-# Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+  # Logging
+  stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
 
-# Set master PID and state locations
-pidfile "#{shared_dir}/pids/puma.pid"
-state_path "#{shared_dir}/pids/puma.state"
-activate_control_app
+  # Set master PID and state locations
+  pidfile "#{shared_dir}/pids/puma.pid"
+  state_path "#{shared_dir}/pids/puma.state"
+  activate_control_app
 
-on_worker_boot do
-  require "active_record"
-  ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
-  ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  on_worker_boot do
+    require "active_record"
+    ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+    ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[rails_env])
+  end
+
+else
+  workers Integer(ENV['WEB_CONCURRENCY'] || 2)
+  threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+  threads threads_count, threads_count
+
+  preload_app!
+
+  rackup      DefaultRackup
+  port        ENV['PORT']     || 3000
+  environment ENV['RACK_ENV'] || 'development'
+
+  on_worker_boot do
+    # Worker specific setup for Rails 4.1+
+    # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
+    ActiveRecord::Base.establish_connection
+  end
 end
